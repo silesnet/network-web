62ce8f5a7833fa3df22230295679dbaf41b74896
robtar <robert.taraba.st@gmail.com>
2015-12-27 16:16:02 +0100

    Add components for IP, MAC, Port inputs

--- js/app-components.js
+++ js/app-components.js
@@ -23,3 +23,63 @@ App.FlashMessageComponent = Ember.Component.extend({
     this.get('flash').destroyMessage();
   }
 });
+
+App.IPAddressField = Ember.TextField.extend({
+  classNames: "form-control ip-address",
+  classNameBindings: ['err'],
+  type: 'text',
+  placeholder: "0.0.0.0",
+  maxlength: "15",
+  keyPress: function (e){
+    if((e.keyCode > 47 && e.keyCode < 58) || e.keyCode == 190 || e.keyCode == 46|| e.keyCode == 8 || e.keyCode == 37|| e.keyCode == 39){
+      return true;
+    }
+    e.preventDefault();
+    return false;
+  },
+  keyUp: function (){
+    var regIpAddress = new RegExp("^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
+    if(this.value.length > 0 && !regIpAddress.test(this.value)){
+      this.set("err", true);
+    }else{
+      this.set("err", false);
+    }
+  }
+});
+
+App.PortField = Ember.TextField.extend({
+  classNames: "form-control port",
+  classNameBindings: ['err'],
+  type: 'number',
+  maxlength: "3",
+  keyUp: function (){
+    if(this.value > 1 && this.value < 255){
+      this.set("err", false);
+    }else{
+      this.set("err", true);
+    }
+  }
+});
+
+App.MACAddressField = Ember.TextField.extend({
+  classNames: "form-control mac-address",
+  classNameBindings: ['err'],
+  type: 'text',
+  placeholder: "AB:CD:EF:01:23:45",
+  maxlength: "17",
+  keyPress: function (e){
+    if((e.keyCode > 47 && e.keyCode < 58) || (e.keyCode > 96 && e.keyCode < 103) || (e.keyCode > 64 && e.keyCode < 71) || e.keyCode == 45|| e.keyCode == 58 || e.keyCode == 46|| e.keyCode == 8 || e.keyCode == 37|| e.keyCode == 39){
+      return true;
+    }
+    e.preventDefault();
+    return false;
+  },
+  keyUp: function (){
+    var regMacAddress = new RegExp("(?:[a-zA-Z0-9]{2}[:-]){5}[a-zA-Z0-9]{2}");
+    if(this.value.length > 0 && !regMacAddress.test(this.value)){
+      this.set("err", true);
+    }else{
+      this.set("err", false);
+    }
+  }
+});

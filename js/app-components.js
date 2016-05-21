81b6be3bd5907f0a178e0fe2a25d8527d74d027e
Sikora, Richard <sikora@silesnet.cz>
2016-05-21 15:44:01 +0200

    Add search autofocus

--- js/app-components.js
+++ js/app-components.js
@@ -9,6 +9,9 @@ App.ModalFormComponent = Ember.Component.extend({
     this.$('.modal').modal().on('hidden.bs.modal', function() {
       this.sendAction('close');
     }.bind(this));
+    Ember.run.scheduleOnce('afterRender', this, function() {
+      $('.autofocus').focus();
+    });
   }.on('didInsertElement')
 });
 
@@ -69,7 +72,7 @@ App.MACAddressField = Ember.TextField.extend({
   placeholder: "AB:CD:EF:01:23:45",
   maxlength: "17",
   keyPress: function (e){
-    var tmpKeyCode = e.charCode || e.keyCode;  
+    var tmpKeyCode = e.charCode || e.keyCode;
     if((tmpKeyCode > 47 && tmpKeyCode < 58) || (tmpKeyCode > 96 && tmpKeyCode < 103) || (tmpKeyCode > 64 && tmpKeyCode < 71) || tmpKeyCode == 45|| tmpKeyCode == 58 || tmpKeyCode == 46|| tmpKeyCode == 8 || tmpKeyCode == 37|| tmpKeyCode == 39){
       return true;
     }
@@ -97,5 +100,5 @@ App.ActiveButtonComponent = Ember.Component.extend({
       val = 0;
     }
     this.set('value', val);
-  }                             
+  }
 });

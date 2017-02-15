bcbbea8d100c9c179fb5aa41a00fc9b86c3d7fde
der3k <richard.sikora@tieto.com>
2017-02-15 08:35:29 +0100

    Add expected data type to handlebar template loader

--- js/load-hbs.js
+++ js/load-hbs.js
@@ -2,12 +2,12 @@ $('script[type="text/x-handlebars-lazy"]').each(function() {
   var name = $(this).attr('data-template-name'),
     src = $(this).attr('data-src');
   if (src && name) {
-    try {
-      $.get(src, function(template) {
-        Ember.TEMPLATES[name] = Ember.Handlebars.compile(template);
-      });
-    } catch(err) {
+    $.get(src, null, null, 'text')
+    .then(function(template) {
+      Ember.TEMPLATES[name] = Ember.Handlebars.compile(template);
+    })
+    .fail(function(err) {
       console.log("unable to load handlebars template lazily '" + name + "': " + err);    
-    }
+    });
   }
 });

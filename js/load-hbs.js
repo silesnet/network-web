4d7d46e0db3f2cb04da4a2cbc3c838fec796d5ff
der3k <richard.sikora@tieto.com>
2017-02-15 08:17:14 +0100

    Add error checking for lazy loading handlebars templates

--- js/load-hbs.js
+++ js/load-hbs.js
@@ -2,8 +2,12 @@ $('script[type="text/x-handlebars-lazy"]').each(function() {
   var name = $(this).attr('data-template-name'),
     src = $(this).attr('data-src');
   if (src && name) {
-    $.get(src, function(template) {
-      Ember.TEMPLATES[name] = Ember.Handlebars.compile(template);
-    });
+    try {
+      $.get(src, function(template) {
+        Ember.TEMPLATES[name] = Ember.Handlebars.compile(template);
+      });
+    } catch(err) {
+      console.log("unable to load handlebars template lazily '" + name + "': " + err);    
+    }
   }
 });

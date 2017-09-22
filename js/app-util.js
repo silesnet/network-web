c5aa0c01726cebb1313f8e8211183419b1294672
der3k <rsi.news@quick.cz>
2017-09-22 08:45:57 +0200

    Improve services search result visibility

--- js/app-util.js
+++ js/app-util.js
@@ -1,3 +1,15 @@
+Ember.Handlebars.helper ('truncate', function (str, len) {
+  if (str.length > len && str.length > 0) {
+      var new_str = str + " ";
+      new_str = str.substr (0, len);
+      new_str = str.substr (0, new_str.lastIndexOf(" "));
+      new_str = (new_str.length > 0) ? new_str : str.substr (0, len);
+
+      return new Ember.Handlebars.SafeString ( new_str +'...' ); 
+  }
+  return str;
+});
+
 function decodeInput(input) {
   return Ember.$('<textarea />').html(input).text();
 }
